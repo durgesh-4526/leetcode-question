1a2e23c9bff9f5f795984f8deb37ac356ccec276
Durgesh Sahu <73772186+durgesh-4526@users.noreply.github.com>
2021-11-30 08:11:06 +0530

    Create 24.cpp

---
+++ 24.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head)
+        {
+            return head;
+        }
+        ListNode * temp = head;
+        while (temp != NULL && temp->next != NULL)
+        {
+            swap(temp->val,temp->next->val);
+            temp = temp->next->next;
+        
+        }
+        return head;
+    }
+};

79ac56b04ffd505842f7b7fcf7ce9260347488b8
Durgesh Sahu <73772186+durgesh-4526@users.noreply.github.com>
2021-12-09 07:03:57 +0530

    Create 1306.cpp

---
+++ 1306.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+       if(start>=arr.size() || start<0 || arr[start]==-100) return false;
+        if(arr[start]==0) return true;
+        int t=arr[start];
+        arr[start]=-100;
+        bool p=canReach(arr,start+t),m=canReach(arr,start-t);
+        arr[start]=t;
+        return p||m;       
+    }
+};

a29b9b8b38d22138f4b5319838d678c7e1876009
Durgesh Sahu <73772186+durgesh-4526@users.noreply.github.com>
2021-12-25 21:24:02 +0530

    Create 227.cpp

---
+++ 227.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int calculate(string s) {
+        s += '+';
+        stack<int>st;
+        int currSum = 0;
+        char sign = '+';
+        for(int i=0;i<s.size();i++){
+            if(isdigit(s[i])){
+                currSum =currSum*10+(s[i]-'0');
+            }
+            else if(s[i] != ' '){
+                if(sign == '-'){
+                    st.push(currSum*(-1));
+                }
+                else if(sign == '+'){
+                    st.push(currSum);
+                }
+                else if(sign == '/'){
+                    int t=st.top();
+                    st.pop();
+                    st.push(t/currSum);
+                }
+                else if(sign == '*'){
+                    int t=st.top();
+                    st.pop();
+                    st.push(t*currSum);
+                }
+                currSum=0;
+                sign=s[i];
+            }
+        }
+        int ans=0;
+        while(st.size()){
+            ans += st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};

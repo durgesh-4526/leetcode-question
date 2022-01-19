e60b864a8ec07eee2e33625b3efc6439d6a3238e
Durgesh Sahu <73772186+durgesh-4526@users.noreply.github.com>
2022-01-19 09:04:22 +0530

    Create 142.cpp

---
+++ 142.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head || !head->next)return NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast)break;
+        }
+        if(slow != fast)return NULL;
+        slow = head;
+        while(slow != fast){
+            
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow; 
+    }
+};
